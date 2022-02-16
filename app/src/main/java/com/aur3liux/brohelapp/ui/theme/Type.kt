3de8ed93419a3b8f2556a0c10a1cb69ab76ec136
AurLopez <aureliux@hotmail.com>
2022-02-16 09:51:24 -0600

    Agregamos tipos de fuentes QUtroSlab

--- app/src/main/java/com/aur3liux/brohelapp/ui/theme/Type.kt
+++ app/src/main/java/com/aur3liux/brohelapp/ui/theme/Type.kt
@@ -2,27 +2,42 @@ package com.aur3liux.brohelapp.ui.theme
 
 import androidx.compose.material.Typography
 import androidx.compose.ui.text.TextStyle
+import androidx.compose.ui.text.font.Font
 import androidx.compose.ui.text.font.FontFamily
 import androidx.compose.ui.text.font.FontWeight
 import androidx.compose.ui.unit.sp
+import com.aur3liux.brohelapp.R
 
-// Set of Material typography styles to start with
+val QuatroSlabRegular = FontFamily(Font(R.font.quatro_slab_regular))
+val QuatroSlabItalica = FontFamily(Font(R.font.quatro_slab_medium_italic))
+
 val Typography = Typography(
     body1 = TextStyle(
         fontFamily = FontFamily.Default,
         fontWeight = FontWeight.Normal,
         fontSize = 16.sp
-    )
-    /* Other default text styles to override
-    button = TextStyle(
-        fontFamily = FontFamily.Default,
-        fontWeight = FontWeight.W500,
-        fontSize = 14.sp
     ),
     caption = TextStyle(
-        fontFamily = FontFamily.Default,
+        fontFamily = QuatroSlabItalica,
         fontWeight = FontWeight.Normal,
         fontSize = 12.sp
+    ),
+
+    h2 = TextStyle(
+        fontFamily = QuatroSlabRegular,
+        fontWeight = FontWeight.SemiBold,
+        fontSize = 20.sp
+    ),
+
+    button = TextStyle(
+        fontFamily = QuatroSlabRegular,
+        fontWeight = FontWeight.W500,
+        fontSize = 19.sp
+    ),
+
+    subtitle1 = TextStyle(
+        fontFamily = QuatroSlabRegular,
+        fontWeight = FontWeight.Bold,
+        fontSize = 17.sp
     )
-    */
 )
